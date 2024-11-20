2229d8446a5f98da73e5f0378ecdcdcbb3eaa090
Manan Vij <manansdsh@gmail.com>
2024-11-20 14:31:45 +0530

    added aws related configrations

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ dotenv.config({ path: process.cwd() + '/src/.env' });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-if (!process.env.DB_HOST_URL || !process.env.DB_PORT || !process.env.DB_PASSWORD || !process.env.DB_USER_NAME) {
-  console.error('Database environment variables are missing mamnanm!');
-  process.exit(1);  // Exit the process if the environment variables are missing
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,9 +11,11 @@ async function bootstrap() {
     origin:'*', // Replace with your frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable credentials (if needed)
+    allowedHeaders: 'Content-Type, Authorization',
   });
   app.useGlobalPipes(new ValidationPipe());
-  console.log(`Server Started at ${process.env.PORT}`)
-  await app.listen(process.env.PORT || 8000);
+  const port = process.env.PORT || 3000
+  console.log(`Server Started at ${port}`)
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
