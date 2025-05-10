99b6f6856db59c03029becb45a81c5aad2c80c86
Manan Vij <manansdsh@gmail.com>
2025-05-10 15:43:07 +0530

    made endpoint changes on patient endpoint to accept filters and pagination

--- src/patient_clinic/patient_clinic.module.ts
+++ src/patient_clinic/patient_clinic.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PatientClinic } from './entity/patient_clinic.entity';
 import { PatientClinicService } from './patient_clinic.service';
 import { ErrorLogModule } from 'src/errorlog/error-log.module';
+import { Appointment } from 'src/appointment/entity/appointment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PatientClinic]), ErrorLogModule],
+  imports: [TypeOrmModule.forFeature([PatientClinic, Appointment]), ErrorLogModule],
   providers: [PatientClinicService],
   exports: [PatientClinicService],
 })
