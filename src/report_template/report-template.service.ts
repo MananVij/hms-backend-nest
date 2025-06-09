bbe657a6eea41b8d2600614b68c9a5ede5364f66
Manan Vij <manansdsh@gmail.com>
2025-06-09 19:59:42 +0530

    resolved reports bug created due to specialisation enum

--- src/report_template/report-template.service.ts
+++ src/report_template/report-template.service.ts
@@ -14,6 +14,7 @@ import {
 import { Doctor } from 'src/doctor/entity/doctor.entity';
 import { ReportType } from './report-template.enum';
 import { ErrorLogService } from 'src/errorlog/error-log.service';
+import { MedicalSpecialization } from 'src/doctor/entity/specialization.enum';
 
 @Injectable()
 export class ReportTemplateService {
@@ -76,7 +77,7 @@ export class ReportTemplateService {
         where: { user: { uid: userId } },
       });
 
-      if (doctor && doctor.specialization.toLowerCase() === 'cardiologist') {
+      if (doctor && doctor.specialization.toLowerCase() === MedicalSpecialization.CARDIOLOGY) {
         return await queryRunner.manager.find(ReportTemplate, {
           where: { type: ReportType.HEART },
         });
